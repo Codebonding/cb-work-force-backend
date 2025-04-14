543617a9a288001aa73251d6f7886c4c05583e5e
codebonding <codebonding@gmail.com>
2025-04-14 09:07:23 +0530

    first commit

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get('/', (req, res) => {
 });
 
 // Uncomment to run locally
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
