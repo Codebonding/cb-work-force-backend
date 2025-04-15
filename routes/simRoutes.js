55a52d4852b142ade43626951aa66b3029323ff2
codebonding <codebonding@gmail.com>
2025-04-15 18:33:46 +0530

    fc

--- routes/simRoutes.js
+++ routes/simRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const simController = require('../controllers/simController');
 const { validateSim } = require('../validation/simValidation');
-const { isApprover } = require('../middleware/isApprover');
+const { authenticate } = require('../middleware/authMiddleware');
 
-router.post('/add', isApprover, validateSim,simController.addSim);
+router.post('/add', authenticate, validateSim,simController.addSim);
 router.get('/',isApprover, simController.getAllSims);
 
 module.exports = router;
